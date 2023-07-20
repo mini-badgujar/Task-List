be3e45b771d5c65f0418d564a85d13172981f2d8
Damini Badgujar <84827841+mini-badgujar@users.noreply.github.com>
2023-07-20 17:51:22 +0530

    Worked on routes and Bable template-index.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+<h1>
+    this is main page in blade template
+</h1>
+
+@isset($name)
+    <div>the name is {{$name}}</div>
+@endisset
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+//creating route and naming it we can check that using php artisan route:list cammand in terminal
+//added bable template index.bable.php and using it for another data.
 Route::get('/', function () {
-    return "main page";
+    return view('index', [
+        'name' => 'damini'
+    ]);
+})->name('Main page');
+
+//another route
+Route::get('/hello', function(){
+    return "Hello this is another page route";
+})->name('hellp');
+
+// dynamic route
+Route::get('/greet/{name}',function($name){
+    return "Hello ".$name ." !";
+});
+// redirecting route
+Route::get('/anything', function(){
+    return redirect('/hello');
+});
+
+//fallback route which will work if none route works
+Route::fallback(function(){
+    return "still got somewhere";
 });
