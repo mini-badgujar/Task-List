5049cb543307ea03e9ddab024f6e9c2318c83e18
Damini Badgujar <84827841+mini-badgujar@users.noreply.github.com>
2023-07-20 19:39:02 +0530

    worked on layouts inheritance

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,31 @@
 <h1>
-    this is main page in blade template
+    List of Task
 </h1>
+{{-- 
+@if(count($tasks)>0)
+    <div>These are tasks title: </div>
+    @foreach($tasks as $task)
+        <div>{{$task->title}}</div>
+    @endforeach
 
-@isset($name)
-    <div>the name is {{$name}}</div>
-@endisset
+
+@else
+    <div>There are no tasks!</div>
+@endif
+
+<br>
+
+@forelse($tasks as $task)
+    <div>{{$task->title}}</div>
+@empty
+    <p>There are no records</p>
+@endforelse
+
+<br> --}}
+
+
+@foreach($tasks as $task)
+    <div>
+        <a href="{{ route('tasks.show', ['id' => $task->id ])}}">{{ $task->title }}</a>
+    </div>
+@endforeach
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
   <?php
-
+use Illuminate\http\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,31 +12,102 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+class Task
+{
+    public function __construct(
+        public int $id,
+        public string $title,
+        public string $description,
+        public ?string $long_description,
+        public bool $completed,
+        public string $created_at,
+        public string $updated_at
+    ) {
+    }
+}
 
+$tasks = [
+    new Task(
+        1,
+        'Buy groceries',
+        'Task 1 description',
+        'Task 1 long description',
+        false,
+        '2023-03-01 12:00:00',
+        '2023-03-01 12:00:00'
+    ),
+    new Task(
+        2,
+        'Sell old stuff',
+        'Task 2 description',
+        null,
+        false,
+        '2023-03-02 12:00:00',
+        '2023-03-02 12:00:00'
+    ),
+    new Task(
+        3,
+        'Learn programming',
+        'Task 3 description',
+        'Task 3 long description',
+        true,
+        '2023-03-03 12:00:00',
+        '2023-03-03 12:00:00'
+    ),
+    new Task(
+        4,
+        'Take dogs for a walk',
+        'Task 4 description',
+        null,
+        false,
+        '2023-03-04 12:00:00',
+        '2023-03-04 12:00:00'
+    ),
+];
 
 //creating route and naming it we can check that using php artisan route:list cammand in terminal
-//added bable template index.bable.php and using it for another data.
-Route::get('/', function () {
+Route::get('/', function(){
+    return redirect()->route('tasks.index');
+});
+
+Route::get('/tasks', function() use($tasks) {
     return view('index', [
-        'name' => 'damini'
+        'tasks' => $tasks
     ]);
-})->name('Main page');
+})->name('tasks.index');
 
-//another route
-Route::get('/hello', function(){
-    return "Hello this is another page route";
-})->name('hellp');
 
-// dynamic route
-Route::get('/greet/{name}',function($name){
-    return "Hello ".$name ." !";
-});
-// redirecting route
-Route::get('/anything', function(){
-    return redirect('/hello');
-});
+// Route::get('/tasks/{id}', function($id){
+//     return "single task";
+// })->name('tasks.show');
 
-//fallback route which will work if none route works
-Route::fallback(function(){
-    return "still got somewhere";
-});
+// Layouts Using Template Inheritence
+Route::get('/tasks/{id}', function($id) use ($tasks){
+    $task = collect($tasks)->firstWhere('id',$id);
+    
+    if(!$task){
+        abort(Response::HTTP_NOT_FOUND);
+    }
+    return view('show',['task'=>$task]);
+})->name('tasks.show');
+
+
+// //another route
+// Route::get('/hello', function(){
+//     return "Hello this is another page route";
+// })->name('hellp');
+
+// // dynamic route
+// Route::get('/greet/{name}',function($name){
+//     return "Hello ".$name ." !";
+// });
+// // redirecting route
+// Route::get('/anything', function(){
+//     return redirect('/hello');
+// });
+
+// //fallback route which will work if none route works
+// Route::fallback(function(){
+//     return "still got somewhere";
+// });
+//
