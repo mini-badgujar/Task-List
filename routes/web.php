5f5bda556e73c794678fb39594bc5c1c10268530
Damini Badgujar <84827841+mini-badgujar@users.noreply.github.com>
2023-07-25 17:18:50 +0530

    [200~Model and Reading Data          - Forms and CSRF Protection

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
   <?php
-use Illuminate\http\Response;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,67 +13,67 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-class Task
-{
-    public function __construct(
-        public int $id,
-        public string $title,
-        public string $description,
-        public ?string $long_description,
-        public bool $completed,
-        public string $created_at,
-        public string $updated_at
-    ) {
-    }
-}
+// class Task
+// {
+//     public function __construct(
+//         public int $id,
+//         public string $title,
+//         public string $description,
+//         public ?string $long_description,
+//         public bool $completed,
+//         public string $created_at,
+//         public string $updated_at
+//     ) {
+//     }
+// }
 
-$tasks = [
-    new Task(
-        1,
-        'Buy groceries',
-        'Task 1 description',
-        'Task 1 long description',
-        false,
-        '2023-03-01 12:00:00',
-        '2023-03-01 12:00:00'
-    ),
-    new Task(
-        2,
-        'Sell old stuff',
-        'Task 2 description',
-        null,
-        false,
-        '2023-03-02 12:00:00',
-        '2023-03-02 12:00:00'
-    ),
-    new Task(
-        3,
-        'Learn programming',
-        'Task 3 description',
-        'Task 3 long description',
-        true,
-        '2023-03-03 12:00:00',
-        '2023-03-03 12:00:00'
-    ),
-    new Task(
-        4,
-        'Take dogs for a walk',
-        'Task 4 description',
-        null,
-        false,
-        '2023-03-04 12:00:00',
-        '2023-03-04 12:00:00'
-    ),
-];
+// $tasks = [
+//     new Task(
+//         1,
+//         'Buy groceries',
+//         'Task 1 description',
+//         'Task 1 long description',
+//         false,
+//         '2023-03-01 12:00:00',
+//         '2023-03-01 12:00:00'
+//     ),
+//     new Task(
+//         2,
+//         'Sell old stuff',
+//         'Task 2 description',
+//         null,
+//         false,
+//         '2023-03-02 12:00:00',
+//         '2023-03-02 12:00:00'
+//     ),
+//     new Task(
+//         3,
+//         'Learn programming',
+//         'Task 3 description',
+//         'Task 3 long description',
+//         true,
+//         '2023-03-03 12:00:00',
+//         '2023-03-03 12:00:00'
+//     ),
+//     new Task(
+//         4,
+//         'Take dogs for a walk',
+//         'Task 4 description',
+//         null,
+//         false,
+//         '2023-03-04 12:00:00',
+//         '2023-03-04 12:00:00'
+//     ),
+// ];
 
 //creating route and naming it we can check that using php artisan route:list cammand in terminal
-Route::get('/', function(){
-    return redirect()->route('tasks.index');
-});
+// Route::get('/', function(){
+//     return redirect()->route('tasks.index');
+// });
 
-Route::get('/tasks', function() use($tasks) {
-    return view('index', ['tasks' => $tasks]);
-})->name('tasks.index');
+// Route::get('/tasks', function() use($tasks) {
+//     return view('index', ['tasks' => $tasks]);
+// })->name('tasks.index');
 
 
 // Route::get('/tasks/{id}', function($id){
@@ -80,14 +81,14 @@ Route::get('/tasks', function() use($tasks) {
 // })->name('tasks.show');
 
 //Layouts Using Template Inheritence
-Route::get('/tasks/{id}', function($id) use ($tasks){
-    $task = collect($tasks)->firstWhere('id',$id);
+// Route::get('/tasks/{id}', function($id) use ($tasks){
+//     $task = collect($tasks)->firstWhere('id',$id);
     
-    if(!$task){
-        abort(Response::HTTP_NOT_FOUND);
-    }
-    return view('show',['task'=>$task]);
-})->name('tasks.show');
+//     if(!$task){
+//         abort(Response::HTTP_NOT_FOUND);
+//     }
+//     return view('show',['task'=>$task]);
+// })->name('tasks.show');
 
 
 // //another route
@@ -109,3 +110,27 @@ Route::get('/tasks/{id}', function($id) use ($tasks){
 //     return "still got somewhere";
 // });
 //
+//_____________Importing data using models and migration______________________________________________________________________________________________________
+
+Route::get('/', function(){
+    return redirect()->route('tasks.index');
+});
+
+Route::get('/tasks', function(){
+    return view('index', [
+        'tasks' =>  \App\Models\Task::latest()->where('completed', true)->get()
+    ]);
+})->name('tasks.index');
+
+Route::view('/tasks/create', 'create')->name('task.create');
+
+Route::get('task/{id}', function($id){
+    
+    return view('show',[
+        'task'=> \App\Models\Task::findOrFail($id)
+    ]);
+})->name('tasks.show');
+
+Route::post('/tasks', function(Request $request){
+    dd($request->all());
+})->name('task.store');
